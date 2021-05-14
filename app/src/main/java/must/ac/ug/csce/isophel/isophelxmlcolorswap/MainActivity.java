c359bebe47db1c47a5915a3f1f843ca160e39797
Natwijuka Isophel <natisox18@gmail.com>
2021-05-14 10:11:34 +0300

    Update MainActivity.java

--- app/src/main/java/must/ac/ug/csce/isophel/isophelxmlcolorswap/MainActivity.java
+++ app/src/main/java/must/ac/ug/csce/isophel/isophelxmlcolorswap/MainActivity.java
@@ -3,18 +3,23 @@ package must.ac.ug.csce.isophel.isophelxmlcolorswap;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity<MyBroadcastReceiver> extends AppCompatActivity {
+    private MyReceiver isanreceiver;
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +27,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView taptext = (TextView) findViewById(R.id.txtcolor);
         Button tapmebtn = (Button) findViewById(R.id.buttontap);
+        IntentFilter intentFilter = new IntentFilter("com.must.ac.ug.csce.Isophel.IsophelProgColorSwap");
+        isanreceiver = new MyReceiver();
+        registerReceiver(isanreceiver, intentFilter);
 
 
-
-        final Random random= new Random();
+        final Random random = new Random();
         tapmebtn.setOnClickListener(v -> {
-            float red= random.nextFloat();
+            float red = random.nextFloat();
 
-            float green= random.nextFloat();
-            float blue= random.nextFloat();
-            taptext.setText("r"+String.valueOf(red)+"   g"+String.valueOf(green+" b"+String.valueOf(blue)  ));
-            taptext.setTextColor(Color.rgb(red,green,blue));
+            float green = random.nextFloat();
+            float blue = random.nextFloat();
+            taptext.setText("r" + String.valueOf(red) + "   g" + String.valueOf(green + " b" + String.valueOf(blue)));
+            taptext.setTextColor(Color.rgb(red, green, blue));
 
 
         });
 
     }
+        class MyReceiver extends BroadcastReceiver {
+            public void onReceive(Context context, Intent intent) {
+                String message = intent.getStringExtra("message");
+
+                if (intent.getAction().equals("com.must.ac.ug.csce.Isophel.IsophelProgColorSwap")) {
+                    // Display message
+                    Toast.makeText(context, "" + message + "New Broad cast", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+
 }
